648b7ffdd3a2d68fc04c7761765623f13f526e56
Sylvain Dazy <sylvain.dazy@hotmail.com>
2023-04-13 01:22:32 +0200

    Test nothing

---
+++ app/src/test/java/fizzbuzz/GeneratorTest.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GeneratorTest {
+    
+    @Test
+    public void testNothing() {
+        assertEquals(1, 1);
+    }
+}
